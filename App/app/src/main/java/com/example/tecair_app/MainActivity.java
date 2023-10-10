731398403cb84aa0a3cc98a89a358f273e461f5d
Devem002 <vega.diego02@gmail.com>
2023-10-09 23:39:12 -0600

    Avance main page

--- App/app/src/main/java/com/example/tecair_app/MainActivity.java
+++ App/app/src/main/java/com/example/tecair_app/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.tecair_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.tecair_app.flights.flight_admin;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickFlights(View view) {
+        Intent intent = new Intent(this, flight_admin.class);
+        startActivity(intent);
     }
 }
