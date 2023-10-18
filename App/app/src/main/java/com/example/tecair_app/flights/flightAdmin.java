7a1c3d9984b3e7220e41dc4b885cd42316cb4c02
Devem002 <vega.diego02@gmail.com>
2023-10-17 21:13:40 -0600

    Calendario

--- App/app/src/main/java/com/example/tecair_app/flights/flightAdmin.java
+++ App/app/src/main/java/com/example/tecair_app/flights/flightAdmin.java
@@ -1,24 +1,78 @@
 package com.example.tecair_app.flights;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.util.Pair;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.tecair_app.MainActivity;
 import com.example.tecair_app.R;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 
-public class flightAdmin extends AppCompatActivity {
+import java.util.Calendar;
 
+public class flightAdmin extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    TextView date;
+    Button dates;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flight_admin);
+
+        Spinner spinner = findViewById(R.id.spiner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.destinos, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        date = findViewById(R.id.date);
+        dates = findViewById(R.id.dates);
+
+        MaterialDatePicker datePicker = MaterialDatePicker.Builder.dateRangePicker().
+                setSelection(Pair.create(MaterialDatePicker.thisMonthInUtcMilliseconds(),MaterialDatePicker.todayInUtcMilliseconds())).build();
+
+        dates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePicker.show(getSupportFragmentManager(), "Date Picker");
+                datePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+                    @Override
+                    public void onPositiveButtonClick(Object selection) {
+                        date.setText(datePicker.getHeaderText());
+                    }
+                });
+            }
+        });
+
     }
+
 
     public void onClickBack(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = (String) adapterView.getItemAtPosition(i);
+
+        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
