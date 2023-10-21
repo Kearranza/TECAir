d0bb80274e7bc70ad652fb90198db427c30186ba
Josue3108 <67967986+Josue3108@users.noreply.github.com>
2023-10-21 03:28:49 -0600

    Abrir y cerrar vuelos

--- WebApp/src/app/flight-closure/flight-closure.component.ts
+++ WebApp/src/app/flight-closure/flight-closure.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Calendar } from '../Interfaces/calendar.interface';
+import { APIService } from '../api.service';
 
 @Component({
   selector: 'app-flight-closure',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./flight-closure.component.css']
 })
 export class FlightClosureComponent {
-  onSubmit() {}
+
+  string:string = '';
+
+  calendar:Calendar = {
+    id_calendario:'',
+    fecha: new Date(),
+    precio:0,
+    id_avion:'',
+    id_vuelo:0,
+    abierto:false,
+    pases:[],
+    promociones:[],
+    facturas:[],
+  }
+
+  constructor(private apiService: APIService) {}
+
+  onSubmit() {
+    this.apiService.getDataECalendario(this.string).subscribe(data => {
+      this.calendar = data;
+    }, error => {
+      console.error('Error:', error);})
+    this.calendar.abierto = false;
+    this.apiService.updateDataCalendario(this.string, this.calendar)  
+  }
+
+
 }
