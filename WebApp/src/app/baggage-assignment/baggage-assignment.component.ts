8bbbc8f1d47c4fff694b74a40a0b7c876bf30561
Kearranza <cbkevin.ant@gmail.com>
2023-10-21 03:47:37 -0600

    maletas visual

--- WebApp/src/app/baggage-assignment/baggage-assignment.component.ts
+++ WebApp/src/app/baggage-assignment/baggage-assignment.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 
+interface ColorInput {
+  id: number;
+}
+
 @Component({
   selector: 'app-baggage-assignment',
   templateUrl: './baggage-assignment.component.html',
   styleUrls: ['./baggage-assignment.component.css']
 })
 export class BaggageAssignmentComponent {
+
+  colors: ColorInput[] = [{ id: 1 }];
+
+  addColorInput() {
+    if (this.colors.length < 3) {
+      const newColorInput: ColorInput = { id: this.colors.length + 1 };
+      this.colors.push(newColorInput);
+    }
+  }
+
   onSubmit(){}
 
 }
