3463e18cbb4dc6548b9c36b3f258726d94cfa63b
Kearranza <cbkevin.ant@gmail.com>
2023-10-21 20:49:13 -0600

    maletas visual listo

--- WebApp/src/app/baggage-assignment/baggage-assignment.component.ts
+++ WebApp/src/app/baggage-assignment/baggage-assignment.component.ts
@@ -12,12 +12,18 @@ interface ColorInput {
 export class BaggageAssignmentComponent {
 
   colors: ColorInput[] = [{ id: 1 }];
+  
+  repeatCount = 1;
 
   addColorInput() {
     if (this.colors.length < 3) {
       const newColorInput: ColorInput = { id: this.colors.length + 1 };
       this.colors.push(newColorInput);
     }
+  }
+
+  repeatCode(num : number) {
+    this.repeatCount += num;
   }
 
   onSubmit(){}
